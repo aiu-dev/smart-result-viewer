dc19992afb8fd8bb8563bd987f273bb8e9119066
abubakar1239 <abubakaribrahimumar806@gmail.com>
2026-04-05 00:12:11 +0100

    uploading repo to vercel

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,10 @@
 import axios from "axios";
 
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5000"
+    : "https://smart-result-viewer.onrender.com";
+
 export const API = axios.create({
-  baseURL: " https://smart-result-viewer.onrender.com  "
+  baseURL: BASE_URL,
 });
